b3d130d3eba363a0f15e1c4040f82302edf466a2
yoyo10231224 <xuyousheng27@gmail.com>
2026-04-03 17:24:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
-
+const _pending = {};
 const app = new Proxy({}, {
   set(obj, key, value){
-    obj[key] = value
-
-    
+    obj[key] = value;
     if(!window._engineTemplatesReady){
-      document.addEventListener("DOMContentLoaded", () => {
-        updateTemplates()
-        obj[key] = value 
-      }, { once: true })
-      return true
+      _pending[key] = value;
+      return true;
     }
-
-    
-    updateTemplates()
-    return true
+    updateTemplates();
+    return true;
   }
-})
+});
 
 function updateTemplates(){
   document.querySelectorAll("*").forEach(el => {
     if(el._template){
-      let html = el._template
+      let html = el._template;
       for(let k in app){
-        html = html.replaceAll(`{${k}}`, app[k])
+        html = html.replaceAll(`{${k}}`, app[k]);
       }
-      el.innerHTML = html
+      el.innerHTML = html;
     }
-  })
+  });
 }
-
 
 window.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("*").forEach(el => {
     if(el.innerHTML.includes("{")){
-      el._template = el.innerHTML
+      el._template = el.innerHTML;
     }
-  })
-  window._engineTemplatesReady = true
-})
+  });
+  window._engineTemplatesReady = true;
+  for(let k in _pending){
+    app[k] = _pending[k];
+  }
+});
